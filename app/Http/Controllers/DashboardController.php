8cb431603ac3f7e466d05738161be5463fee9e2d
adithyaric <naughtyadit@gmail.com>
2023-07-31 14:48:43 +0700

    Graphs on Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,20 +3,83 @@
 namespace App\Http\Controllers;
 
 use App\Models\Penjualan;
+use App\Models\PenjualanItem;
 use App\Models\Product;
 use App\Models\Slider;
 use App\Models\Stock;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $bestBuyProducts = PenjualanItem::select('product_id', DB::raw('SUM(qty) as total_qty'))
+            ->with('product')
+            ->groupBy('product_id')
+            ->orderBy('total_qty', 'desc')
+            ->take(10)
+            ->get();
+
+        $bestBuySuppliers = PenjualanItem::select('supplier_id', DB::raw('SUM(qty) as total_qty'), 'suppliers.name as supplier_name')
+            ->join('products', 'penjualan_items.product_id', '=', 'products.id')
+            ->join('suppliers', 'products.supplier_id', '=', 'suppliers.id')
+            ->groupBy('supplier_id')
+            ->orderBy('total_qty', 'desc')
+            ->take(10)
+            ->get();
+
+        $salesGraph = Penjualan::select(DB::raw('DATE(created_at) as date'), DB::raw('SUM(total) as total_sales'))
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get()
+            ->map(function ($item) {
+                $item->date = Carbon::parse($item->date)->format('d-M-Y');
+
+                return $item;
+            });
+
+        $productGraph = PenjualanItem::select(DB::raw('DATE(penjualan_items.created_at) as date'), 'products.name as product_name', DB::raw('SUM(qty) as total_qty'))
+            ->join('penjualans', 'penjualan_items.penjualan_id', '=', 'penjualans.id')
+            ->join('products', 'penjualan_items.product_id', '=', 'products.id')
+            ->groupBy('date', 'product_name')
+            ->orderBy('date')
+            ->get()
+            ->map(function ($item) {
+                $item->date = Carbon::parse($item->date)->format('d-M-Y');
+
+                return $item;
+            });
+
+        $monthlyRevenue = Penjualan::select(
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw("DATE_FORMAT(created_at, '%M') as month"),
+            DB::raw('SUM(total) as total')
+        )
+            ->groupBy('year', 'month')
+            ->orderBy('year', 'asc')
+            ->orderByRaw("FIELD(month, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')")
+            ->get();
+
+        if ($request->wantsJson()) {
+            // Return the data as a JSON response
+            return response()->json([
+                'bestBuyProducts' => $bestBuyProducts,
+                'bestBuySuppliers' => $bestBuySuppliers,
+                'salesGraph' => $salesGraph,
+                'productGraph' => $productGraph,
+                'monthlyRevenue' => $monthlyRevenue,
+            ]);
+        }
+
         return view('dashboard.index', [
-            'users' => User::count(),
+            // 'users' => User::count(),
             'products' => Product::count(),
             'stocks' => Stock::count(),
             'penjualans' => Penjualan::count(),
+            'totalRevenue' => $monthlyRevenue->sum('total'),
             // 'sliders' => Slider::where('status', 'active')->get(),
         ]);
     }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,16 +11,71 @@
 
     <section class="content">
         <div class="row">
-            <div class="col-lg-12 col-xs-12">
+            <div class="col-lg-3 col-xs-3">
                 <div class="small-box bg-yellow-gradient">
                     <div class="inner">
-                        <p style="font-size:20px;">{{ $users }}</p>
-                        <p>Jumlah Users</p>
+                        <p style="font-size:20px;">{{ $products }}</p>
+                        <p>Jumlah Total products</p>
                     </div>
                     <div class="icon">
-                        <i class="ion-android-contacts"></i>
+                        <i class="fa fa-archive"></i>
                     </div>
                 </div>
+            </div>
+            <div class="col-lg-3 col-xs-3">
+                <div class="small-box bg-yellow-gradient">
+                    <div class="inner">
+                        <p style="font-size:20px;">{{ $stocks }}</p>
+                        <p>Jumlah Total stocks</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-cubes"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-xs-3">
+                <div class="small-box bg-yellow-gradient">
+                    <div class="inner">
+                        <p style="font-size:20px;">{{ $penjualans }}</p>
+                        <p>Jumlah Total Penjualan</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-calculator"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-xs-3">
+                <div class="small-box bg-yellow-gradient">
+                    <div class="inner">
+                        <p style="font-size:20px;">@currency($totalRevenue)</p>
+                        <p>Jumlah Total Pendapatan</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-dollar"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <div id="bestBuyProducts"></div>
+            </div>
+            {{-- <div class="col-lg-6 col-xs-6"> --}}
+            {{-- <div id="bestOutlets"></div> --}}
+            {{-- </div> --}}
+            <div class="col-lg-6 col-xs-6">
+                <div id="bestBuySuppliers"></div>
+                <hr />
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <div id="salesGraph"></div>
+                <hr />
+            </div>
+            <div class="col-lg-6 col-xs-6">
+                <div id="monthlyRevenue"></div>
+                <hr />
+            </div>
+            <div class="col-lg-12 col-xs-12">
+                <div id="productGraph"></div>
+                <hr />
             </div>
             {{-- <div class="col-lg-12 col-xs-12"> --}}
             {{-- <h1 for="check-sliders">Sliders</h1> --}}
@@ -30,4 +85,220 @@
             {{-- </div> --}}
         </div>
     </section>
+@endsection
+@section('page-script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/highcharts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/modules/exporting.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/modules/export-data.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.3/modules/accessibility.min.js"></script>
+    <script>
+        $.ajax({
+            url: '/dashboard',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                // Use the data returned from the server to generate the graphs
+                let bestBuyProducts = data.bestBuyProducts;
+                let bestBuySuppliers = data.bestBuySuppliers;
+                let salesGraph = data.salesGraph;
+                let productGraph = data.productGraph;
+                let monthlyRevenue = data.monthlyRevenue;
+                console.table(bestBuyProducts);
+                console.table(bestBuySuppliers);
+                console.table(salesGraph);
+                console.table(productGraph);
+                console.table(monthlyRevenue);
+
+                // 1. Grafik 10 Product Best Buy
+                Highcharts.chart('bestBuyProducts', {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: '10 Product Best Buy'
+                    },
+                    xAxis: {
+                        categories: bestBuyProducts.map(item => item.product.name)
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Total Quantity'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        },
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y;
+                        }
+                    },
+                    series: [{
+                        name: 'Quantity',
+                        colorByPoint: true,
+                        data: bestBuyProducts.map(item => item.total_qty).map(Number)
+                    }]
+                });
+
+                // 2. Grafik 10 Outlet terbaik
+
+                // 3. Grafik 10 Supplier Best Buy
+                Highcharts.chart('bestBuySuppliers', {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: '10 Best Buy Suppliers'
+                    },
+                    xAxis: {
+                        categories: bestBuySuppliers.map(item => item.supplier_name)
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Total Quantity'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        },
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y;
+                        }
+                    },
+                    series: [{
+                        name: 'Quantity',
+                        data: bestBuySuppliers.map(item => item.total_qty).map(Number)
+                    }]
+                });
+
+                // 4. Grafik Penjualan
+                Highcharts.chart('salesGraph', {
+                    chart: {
+                        type: 'line'
+                    },
+                    title: {
+                        text: 'Sales Graph'
+                    },
+                    xAxis: {
+                        categories: salesGraph.map(item => item.date)
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Total Sales'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        },
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y;
+                        }
+                    },
+                    series: [{
+                        name: 'Sales',
+                        colorByPoint: true,
+                        data: salesGraph.map(item => item.total_sales).map(Number)
+                    }]
+                });
+
+                // 5. Grafik Product
+                Highcharts.chart('productGraph', {
+                    chart: {
+                        type: 'line'
+                    },
+                    title: {
+                        text: 'Product Graph'
+                    },
+                    xAxis: {
+                        categories: productGraph.map(item => item.product_name + ' (' + item.date + ')')
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Total Quantity'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        }
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            // Find the data item for the current point
+                            let item = productGraph.find(item => item.date === this.x && item
+                                .product_name === this.series.name);
+
+                            // Return the formatted tooltip text
+                            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y +
+                                '<br/>Product: ' + item.product_name;
+                        }
+                    },
+                    series: [{
+                        name: 'Quantity',
+                        colorByPoint: true,
+                        data: productGraph.map(item => item.total_qty).map(Number)
+                    }]
+                });
+
+                // 6. Grafik Pendapatan Perbulan
+                Highcharts.chart('monthlyRevenue', {
+                    chart: {
+                        type: 'line'
+                    },
+                    title: {
+                        text: 'Sales Graph Monthly'
+                    },
+                    xAxis: {
+                        categories: monthlyRevenue.map(item => item.month + ' ' + item.year)
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Total Sales'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            dataLabels: {
+                                enabled: true
+                            }
+                        },
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y;
+                        }
+                    },
+                    series: [{
+                        name: 'Sales',
+                        colorByPoint: true,
+                        data: monthlyRevenue.map(item => item.total).map(Number)
+                    }]
+                });
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // Handle any errors that occur
+            }
+        });
+    </script>
 @endsection
